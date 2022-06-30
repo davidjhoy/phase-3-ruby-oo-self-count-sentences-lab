b8886834344001dd575a7a5ef2b9c1df77842ea3
david <davidjhoyjr@gmail.com>
2022-06-30 12:11:40 -0400

    commit tree af5c66844be8490f1f2c5fbcb678ed12ee962c0c

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,34 @@ require 'pry'
 class String
 
   def sentence?
+    if self[-1] === "."
+      return true
+    else 
+      return false
+    end
+
     
   end
 
   def question?
+    if self[-1] === "?"
+      return true
+    else 
+      return false
+    end
 
   end
 
   def exclamation?
+    if self[-1] === "!"
+      return true
+    else 
+      return false
+    end
 
   end
 
   def count_sentences
-
+    self.split(/\.|\?|\!/).filter{|item| item.length > 1}.length
   end
 end
